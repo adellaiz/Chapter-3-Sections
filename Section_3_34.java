774b4180002a18c6517b683f22fb0133f7a7de9d
adellaiz <68011080+adellaiz@users.noreply.github.com>
2020-07-10 11:53:41 +0700

    Update Section_3_34.java

--- Section_3_34.java
+++ Section_3_34.java
@@ -3,7 +3,11 @@
  * @email [adellaiz@student.ub.ac.id]
  * @create date 2020-07-10 10:54:09
  * @modify date 2020-07-10 11:14:22
- * @desc [description]
+ * @desc [(Geometry: point on line segment) Programming Exercise 3.32 shows how to test
+whether a point is on an unbounded line. Revise Programming Exercise 3.32 to
+test whether a point is on a line segment. Write a program that prompts the user
+to enter the three points for p0, p1, and p2 and displays whether p2 is on the line
+segment from p0 to p1.]
  */
 package Chapter_3_Section;
 import java.util.Scanner;
